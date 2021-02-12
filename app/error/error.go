b388bb98f1f5d8c308b1ad698d279243e435fa8e
satorunooshie <rule.the.fate.myfirststory@gmail.com>
2021-02-12 23:50:14 +0900

    [fix]undefined response status code and created StatusCode405()

--- app/error/error.go
+++ app/error/error.go
@@ -1,5 +1,23 @@
 package error
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
 type Message struct {
 	Message string `json:"message"`
+}
+
+func StatusCode405(w http.ResponseWriter) http.ResponseWriter {
+	// generate json message for response
+	m := Message{"method not allowed"}
+	res, err := json.Marshal(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.WriteHeader(http.StatusMethodNotAllowed)
+	w.Write(res)
+	return w
 }
